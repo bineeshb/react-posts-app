0fedd7bd3f4a0318cd6a6eeaa29b409e101b91e0
Dinesh Kumar K <dineshmail.kannan@gmail.com>
2024-03-25 12:02:24 +0530

    feature: post component implementation (#12)
    Co-authored-by: DINESH <dinesh@DINESHs-MacBook-Pro.local>

---
+++ src/Components/Post.js
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Tag from "./Tag";
+
+const Post = (props) => {
+    const { post } = props;
+    return (
+        post && (
+            <div className="m-2 p-2 flex justify-between h-32 rounded-md bg-slate-50">
+                <div>
+                    <h3 className="text-base font-semibold text-gray-900 opacity-100">
+                        {post?.title}
+                    </h3>
+                    <p className="my-2 mr-2 text-sm line-clamp-2 text-gray-500">
+                        {post?.body}
+                    </p>
+                    <div className="mt-4">
+                        {post?.tags?.map((tag) => {
+                            return <Tag sentiment="success">{tag}</Tag>;
+                        })}
+                    </div>
+                </div>
+                <div className="flex justify-start align-bottom">
+                    <FontAwesomeIcon
+                        icon="fa-heart"
+                        className=" text-pink-400"
+                    />
+                    <div className="mt-[-4px]  text-gray-500">
+                        {post?.reactions}
+                    </div>
+                </div>
+            </div>
+        )
+    );
+};
+
+export default Post;
